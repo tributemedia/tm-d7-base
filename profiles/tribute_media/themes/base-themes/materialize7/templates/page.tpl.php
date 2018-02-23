e3e8aeea914169a0bd78c1f734eea0a030e76637
Anthony Johnson <aj72325@gmail.com>
2018-02-23 11:11:44 -0700

    Fixing Menu Disappearing on iOS
    There is a style that targets .ios #header with a display:none property that makes the mobile menu disappear on tablet devices. Don fixed this by re-naming the header ID.

--- profiles/tribute_media/themes/base-themes/materialize7/templates/page.tpl.php
+++ profiles/tribute_media/themes/base-themes/materialize7/templates/page.tpl.php
@@ -28,7 +28,7 @@ if ($powered_by == '1') {
 <div id="top" class="materialize d7">
   <div class="page">
     <div class="page-inner">
-        
+
       <div class="top-wrapper w-navigation">
         <div class="top-wrapper wo-navigation">
 
@@ -40,36 +40,36 @@ if ($powered_by == '1') {
                     <?php print render($page['help']); ?>
                   </div>
                 </div> <!-- #help-container -->
-              </div> 
+              </div>
             </div> <!-- .help-wrapper -->
           <?php endif; ?>
-                  
+
           <header id="header-wrapper" class="header-wrapper wrapper" role="banner">
             <div class="inner">
               <div id="header-container" class="container">
                 <div class="inner row">
-                  
+
                   <!-- branding -->
                   <div id="site-info" class="site-info col s11 m3">
                     <div class="inner">
                          <?php if ($logo): ?><div class="logo"><a class="logo" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a></div> <!-- .logo --><?php endif; ?>
                     </div> <!-- .inner -->
                   </div> <!-- .site-info -->
-                  
+
                   <!-- header block -->
-                  <div id="header" class="header-region region col s12 m8 hide-on-med-and-down">
+                  <div id="header-div" class="header-region region col s12 m8 hide-on-med-and-down">
                     <div class="inner">
                       <?php print render($page['header']); ?>
                     </div>
                   </div> <!-- #header -->
-                  
+
                 </div> <!-- .header-inner -->
               </div> <!-- #header-container -->
             </div> <!-- .header-wrapper-inner -->
           </header> <!-- #header-wrapper -->
-          
+
         </div> <!-- .top-wrapper .wo-navigation-->
-        
+
         <?php if ($main_menu): ?>
           <nav id="main-navigation-wrapper" class="main-navigation-wrapper wrapper">
             <div class="shrinking-icon"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="logo-icon"><img alt="<?php print t('Home'); ?>" src="/<?php $theme_path = drupal_get_path('theme', variable_get('theme_default', NULL)); echo $theme_path; ?>/logo-icon.png"></a></div>
@@ -91,8 +91,8 @@ if ($powered_by == '1') {
             </div> <!-- .main-navigation -->
           </nav> <!-- .main-navigation-wrapper -->
         <?php endif; ?> <!-- end main menu -->
-        
-        
+
+
         <?php if (!empty($page['mega_menu'])): ?>
           <nav id="mega-menu-wrapper" class="mega-menu-wrapper wrapper">
             <div class="shrinking-icon"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="logo-icon"><img alt="<?php print t('Home'); ?>" src="/<?php $theme_path = drupal_get_path('theme', variable_get('theme_default', NULL)); echo $theme_path; ?>/logo-icon.png"></a></div>
@@ -105,7 +105,7 @@ if ($powered_by == '1') {
           </div>
         </nav>
         <?php endif; ?>
-        
+
       </div> <!-- .top-wrapper .w-navigation-->
 
 
@@ -124,7 +124,7 @@ if ($powered_by == '1') {
 
       <div id="content-area-wrapper" class="content-area-wrapper">
         <div class="inner">
-        
+
         <?php if ($page['preface']): ?>
           <section id="preface-wrapper" class="preface-wrapper wrapper">
             <div class="inner">
@@ -138,12 +138,12 @@ if ($powered_by == '1') {
             </div>
           </section>
         <?php endif; ?>
-        
+
         <div id="content-wrapper" class="content-wrapper wrapper">
           <div class="inner">
             <div id="content-container" class="container">
               <div class="inner row">
-                
+
                 <main class="content-region col s12<?php if (!empty($page['sidebar_second'])): ?> m7 l8<?php endif; ?>" role="main">
                   <a id="main-content"></a>
                     <div class="inner">
@@ -153,28 +153,28 @@ if ($powered_by == '1') {
                         <?php endif; ?>
 
                         <?php print render($title_prefix); ?>
-                
+
                         <?php if (!empty($title)): ?>
                           <h1 class="page-header title"><?php print $title; ?></h1>
                         <?php endif; ?>
-                
+
                         <?php print render($title_suffix); ?>
-                
+
                         <?php print $messages; ?>
 
                         <?php if (!empty($action_links)): ?>
                           <ul class="action-links"><?php print render($action_links); ?></ul>
                         <?php endif; ?>
-                
+
                         <?php print render($page['content']); ?>
-                
+
                         <?php if(!empty($feed_icons)): ?>
                           <div class="rssFeed"><?php print $feed_icons; ?></div>
                         <?php endif; ?>
                     </div><!-- .layout-content -->
                   </div>
                 </main> <!-- .content-region -->
-              
+
                   <?php if (!empty($page['sidebar_second'])): ?>
                     <aside class="sidebar-second sidebar region layout-sidebar-second col s12 m5 l4" role="complementary">
                       <div class="inner">
@@ -182,14 +182,14 @@ if ($powered_by == '1') {
                       </div> <!-- .inner -->
                     </aside>  <!-- #sidebar-second -->
                   <?php endif; ?>
-              
-                </div> <!-- inner row --> 
+
+                </div> <!-- inner row -->
               </div> <!-- #content-container -->
             </div>
           </div> <!-- #content-wrapper -->
         </div>
       </div><!-- #content-area-wrapper -->
-      
+
       <?php if ($page['postscript']): ?>
         <section id="postscript-wrapper" class="preface-wrapper wrapper">
           <div class="inner">
@@ -208,7 +208,7 @@ if ($powered_by == '1') {
 	<div id="footer-wrapper">
 		<!-- <div class="inner"> -->
 		<?php if ($page['footer']): ?>
-			<div class="footer-region container">		
+			<div class="footer-region container">
             	<?php print render($page['footer']); ?>
 			</div>
 		<?php endif; ?>
@@ -217,7 +217,7 @@ if ($powered_by == '1') {
 				<div>&copy;<?php echo date("Y");?>&nbsp;</div>
 				<div itemprop="name"><?php print $site_name; ?></div>
 			</div>
-			
+
 			<div class="connect">
 				<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
 					<div itemprop="streetAddress"><?php print $street_address; ?></div>
@@ -240,7 +240,7 @@ if ($powered_by == '1') {
 	  <?php if ($powered_by != '2') { ?>
     <div id="powered-by" class="right-align center-on-mobile">
       <div class="inner">
-      <?php 
+      <?php
       print $pb_text;
      	if ($logged_in) {
         print ' | ' . l(t('logout'), '/user/logout');
@@ -253,18 +253,18 @@ if ($powered_by == '1') {
     <?php } ?>
 		</div> <!-- #footer-wrapper -->
 </footer>
-    
+
     <?php if (!empty($page['mob'])): ?>
                 <?php print render($page['mob']); ?>
 
     <?php endif; ?>
-    
-      
+
+
     </div> <!-- .page-inner -->
-    
+
     <div id="back-to-top-wrapper">
       <a href="#top" id="back-to-top" class="back-to-top waves-effect waves-light">Top <i class="fa fa-arrow-circle-up"> </i></a>
-    </div>  
+    </div>
   </div> <!-- .page -->
 </div> <!-- .materialize -->
 
