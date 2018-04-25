b4ac9021118db11f67e823c73a4d06962f451be7
Steve Kenley <skenley@tributemedia.com>
2018-04-25 15:27:14 -0600

    Revert "Merge branch 'core-update'"
    This reverts commit 671a849715a4c9e0fe1083a7e21c7f5335b69d5b, reversing
    changes made to 18e8816e7ba00baf598618be649af468cc821e9c.

--- profiles/tribute_media/themes/base-themes/materialize7/js/custom.js
+++ profiles/tribute_media/themes/base-themes/materialize7/js/custom.js
@@ -1,10 +1,6 @@
 ;(function($){
     Drupal.behaviors.custom_materialize = {
-<<<<<<< HEAD
     attach: function (context, settings) {
-=======
-    attach: function (context, settings) {	        
->>>>>>> core-update
 
         $('.button').addClass('waves-light btn waves-effect');
         $('.webform-submit').addClass('waves-light btn waves-effect button');
@@ -14,15 +10,9 @@
         $('.field-name-field-tags a').addClass('waves-light waves-effect');
         $('.node-readmore').addClass('waves-dark waves-effect btn');
         $('.more-link').addClass('waves-dark waves-effect btn');
-<<<<<<< HEAD
 
         $('.parallax').parallax();
 
-=======
-        
-        $('.parallax').parallax();
-        
->>>>>>> core-update
         $(document).ready(function(){
         	$('a[href^="#"]').on('click',function (e) {
         	    e.preventDefault();
@@ -31,21 +21,12 @@
         	    var $target = $(target);
 
               $('html, body').stop().animate({
-<<<<<<< HEAD
                    'scrollTop': $target.offset().top - $('.top-wrapper.wo-navigation').height()
               }, 900, 'swing');
         	});
         });
 
         //body on scroll
-=======
-                   'scrollTop': $target.offset().top
-              }, 900, 'swing');
-        	});
-        });
-        
-        //body on scroll 
->>>>>>> core-update
         $(document).on("scroll", function(){
             if($(document).scrollTop() > 100){
               $(".page-inner").addClass("shrink"),
@@ -54,11 +35,7 @@
               $("#main-navigation-wrapper").addClass("shrink");
             }
             else
-<<<<<<< HEAD
             {
-=======
-            {                
->>>>>>> core-update
               $(".page-inner").removeClass("shrink"),
               $("header").removeClass("shrink"),
               $("#content-area-wrappr").removeClass("shrink"),
@@ -66,11 +43,7 @@
             };
 
         });
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> core-update
         $(document).on("scroll", function(){
             if($(document).scrollTop() > 500){
              $("#back-to-top-wrapper").addClass("show");
@@ -80,19 +53,11 @@
               $("#back-to-top-wrapper").removeClass("show");
             };
         });
-<<<<<<< HEAD
 
 
 
 
     }
-=======
-        
-
-        
-        
-    }    
->>>>>>> core-update
  };
 
 })(jQuery);
