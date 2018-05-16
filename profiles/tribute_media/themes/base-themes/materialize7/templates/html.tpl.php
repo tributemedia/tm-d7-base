1994f45f9a13ef153d96f223b0116d73b725493a
Don Elliott <don@Dons-MacBook-Pro-3.local>
2018-05-16 10:48:54 -0600

    Updating HTML.TPL.PHP file with paths to additional icon files.

--- profiles/tribute_media/themes/base-themes/materialize7/templates/html.tpl.php
+++ profiles/tribute_media/themes/base-themes/materialize7/templates/html.tpl.php
@@ -62,6 +62,22 @@ xml:lang="en"
   <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
+	  <link rel="apple-touch-icon" sizes="57x57" href="../../../../../sites/all/themes/material_custom/apple-icon-57x57.png">
+	  <link rel="apple-touch-icon" sizes="60x60" href="../../../../../sites/all/themes/material_custom/apple-icon-60x60.png">
+	  <link rel="apple-touch-icon" sizes="72x72" href="../../../../../sites/all/themes/material_custom/apple-icon-72x72.png">
+	  <link rel="apple-touch-icon" sizes="76x76" href="../../../../../sites/all/themes/material_custom/apple-icon-76x76.png">
+	  <link rel="apple-touch-icon" sizes="114x114" href="../../../../../sites/all/themes/material_custom/apple-icon-114x114.png">
+	  <link rel="apple-touch-icon" sizes="120x120" href="../../../../../sites/all/themes/material_custom/apple-icon-120x120.png">
+	  <link rel="apple-touch-icon" sizes="144x144" href="../../../../../sites/all/themes/material_custom/apple-icon-144x144.png">
+	  <link rel="apple-touch-icon" sizes="152x152" href="../../../../../sites/all/themes/material_custom/apple-icon-152x152.png">
+	  <link rel="apple-touch-icon" sizes="180x180" href="../../../../../sites/all/themes/material_custom/apple-icon-180x180.png">
+	  <link rel="icon" type="image/png" sizes="192x192"  href="../../../../../sites/all/themes/material_custom/android-icon-192x192.png">
+	  <link rel="icon" type="image/png" sizes="32x32" href="../../../../../sites/all/themes/material_custom/favicon-32x32.png">
+	  <link rel="icon" type="image/png" sizes="96x96" href="../../../../../sites/all/themes/material_custom/favicon-96x96.png">
+	  <link rel="icon" type="image/png" sizes="16x16" href="../../../../../sites/all/themes/material_custom/favicon-16x16.png">
+	  <meta name="msapplication-TileColor" content="#ffffff">
+	  <meta name="msapplication-TileImage" content="../../../../../sites/all/themes/material_custom/ms-icon-144x144.png">
+	  <meta name="theme-color" content="#ffffff">
   <?php print $scripts; ?>
   <script>
   jQuery(document).ready(function($){
